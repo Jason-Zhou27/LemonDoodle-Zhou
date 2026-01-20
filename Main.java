313247074edbb0bdfccf7dd37cddc0204a9b6faf
Zhou <zhoujas27@7hills.org>
2026-01-20 08:47:50 -0500

    created cup with two ovals and trapezoid

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 import doodlepad.*;
+import java.util.ArrayList;
 
 public class Main {
 	//variables
 	//method
 	public static void main(String[] args){
-		
+		//stand structure
 		Pad pad = new Pad("Stand", 600, 600);
 		RoundRect r1 = new RoundRect(10, 10, 560, 550, 30, 30);
 		Image im = new Image("lemonadeName.jpg", 200, 280, 180, 115);
-		Image imCup = new Image("redCup.png", 200, 420, 80, 85);
 		Line l1 = new Line(100, 300, 100, 560);
 		Line l2 = new Line(500, 300, 500, 560);
 		Line l3 = new Line(100, 300, 500, 300);
@@ -17,6 +17,19 @@ public class Main {
 		Line l6 = new Line(250, 225, 250, 300);
 		Line l7 = new Line(350, 225, 350, 300);
 		Line l8 = new Line(250, 225, 350, 225);
+
+		ArrayList<Point> cupBody = new ArrayList<>();
+        cupBody.add( new Point(  200.0, 440.0) );
+        cupBody.add( new Point( 250.0,  440.0) );
+        cupBody.add( new Point( 240.0,  480.0) );
+        cupBody.add( new Point( 210.0, 480.0) );
+        
+        // Pass Point List to Polygon constructor
+        Polygon p1 = new Polygon(cupBody);
+        Oval o1 = new Oval(200, 420, 50, 30);
+        Oval o2 = new Oval(210, 470, 30, 20);
+
+		
        
 	
 	}
